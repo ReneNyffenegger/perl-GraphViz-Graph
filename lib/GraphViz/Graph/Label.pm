52f2601e53470b06ec8c29b6ffaf05f8692cc0ca
René Nyffenegger <mail@renenyffenegger.ch>
2017-07-11 10:51:03 +0200

    Sanity checks for constructor of Label

--- lib/GraphViz/Graph/Label.pm
+++ lib/GraphViz/Graph/Label.pm
@@ -1,27 +1,53 @@
 package GraphViz::Graph::Label;
 
+use Carp;
+
 sub new { #_{
 
   my $class = shift;
   my $opts  = shift;
   my $self = {};
 
+  croak 'Options expected'       unless defined $opts;
+  croak 'Options must be a HASH' unless ref $opts eq 'HASH';
+
   if ($opts->{text}) {
-    $self->{text} = delete $opts->{text};
+      $self->{type} = 'text';
+      $self->{text_or_html} = delete $opts->{text};
   }
+  elsif ($opts->{html}) {
+      $self->{type} = 'html';
+      $self->{text_or_html} = delete $opts->{html};
+  }
+  else {
+      croak "A label must be either a text or a html label";
+  }
+
+  croak "Unrecognized opts " . join "/", keys %$opts if keys %$opts;
 
   bless $self, $class;
   return $self;
 
 } #_}
 
-sub dot_text {
+sub dot_text { #_{
 
   my $self = shift;
-  return "
-  label=\"$self->{text}\"
+
+  if ($self->{type} eq 'text') {
+
+     return "
+  label=\"$self->{text_or_html}\"
 ";
 
-}
+  }
+  else {
+     return "
+  label=<$self->{text_or_html}>
+";
+
+  }
+
+} #_}
 
 'tq84';
